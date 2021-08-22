265f211ed13c930071fb4ca4f6f9e1406b253247
Alana Cunha <alana.cunha@gmail.com>
2021-08-22 12:57:13 -0400

    commit selectRowTest

--- src/test/java/Tests/SelectionRowsTest.java
+++ src/test/java/Tests/SelectionRowsTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class SelectionRowsTest {
@@ -21,12 +23,12 @@ public class SelectionRowsTest {
         browser.get("http://the-internet.herokuapp.com/large");
         browser.manage().window().maximize();
 
-        //WebElement SelectRow = browser.findElement(By.className("example"));
+        WebElement SelectRow = browser.findElement(By.className("example"));
 
         //SelectRow.findElements(By.xpath("//div[@class='example']/h4[2]"));
 
-        Select SelectRow = new Select(browser.findElement(By.xpath("//div[@class='example']/h4[2]")));
-        SelectRow.selectByVisibleText ("Siblings");
-
+        Actions action = new Actions(browser);
+        action.doubleClick (SelectRow.findElement(By.xpath("//div[@class='example']/h4[2]"))).perform();
+        action.doubleClick (SelectRow.findElement(By.xpath("//div[@id='sibling-13.2']"))).perform();
     }
 }
--- src/test/java/Tests/UserCredentialsTest.java
+++ src/test/java/Tests/UserCredentialsTest.java
@@ -1,13 +1,18 @@
 package Tests;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//@RunWith(DataDrivenTestRunner);
+//@DataLoader(filePaths = "UserCredentialsTest.csv");
 public class UserCredentialsTest {
     private WebDriver browser;
 
